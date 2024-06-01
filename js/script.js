968a21a72010c70c6fb4b51d5162d56a55591df7
ClaudioScordari <scordari22@gmail.com>
2024-06-01 20:27:34 +0200

    Aggiunta funzione di vittoria

--- js/script.js
+++ js/script.js
@@ -17,6 +17,11 @@
         con quante bombe vuole giocare
         - validazione per il numero delle bombe (tra 1 a 99)
 
+    6 - vittoria
+        - serve un counter che dovrà andare finchè non beccha la bomba
+          se io faccio l'array delle bombe - 100 (caselle totali), so quali sono le caselle pulite
+          quando il counter arriva a quel numero ho vinto
+
 */
 
 const { createApp } = Vue
@@ -28,17 +33,15 @@ createApp({
             1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,
             13,  14,  15,  16,  17,  18,  19,  20,  21,  22,  23,  24,  25,
             26,  27,  28,  29,  30,  31,  32,  33,  34,  35,  36,  37,  38,
-            39,  40,  41,  42,  43,  44,  45,  46,  47,  48,  49,  50,  51,
-            52,  53,  54,  55,  56,  57,  58,  59,  60,  61,  62,  63,  64,
-            65,  66,  67,  68,  69,  70,  71,  72,  73,  74,  75,  76,  77,
-            78,  79,  80,  81,  82,  83,  84,  85,  86,  87,  88,  89,  90,
-            91,  92,  93,  94,  95,  96,  97,  98, 100
+            39,  40,  41,  42,  43,  44,  45,  46,  47,  48,  49,  50,
         ],
         arrayBombs: [],
         gameOver: false,
         foundBomb: false,
         start: false,
-        inputBombs: 15,
+        inputBombs: 25,
+        counter: 0,
+        goodSqaures: 0,
     }
   },
   methods: {
@@ -46,9 +49,12 @@ createApp({
     startGame(){
 
         // riempio l'array delle bombe + validazione
-        if (this.inputBombs != 0 && this.inputBombs != 100) {
+        if (this.inputBombs != 0 && this.inputBombs != 50) {
             this.setBombs(this.inputBombs);
             this.start = true;
+
+            this.goodSqaures = 49 - this.arrayBombs.length;
+            console.log('Inizio gioco - caselle buone sono ' + this.goodSqaures);
         }
         else {
             alert('Le bombe possono essere da 1 a 99');
@@ -59,7 +65,7 @@ createApp({
         while (this.arrayBombs.length < this.inputBombs) {
 
             // si arriva ad un massimo di 100 il valor che può avere la casella con la bomba
-            let nBombs = Math.floor(Math.random() * 100) + 1; 
+            let nBombs = Math.floor(Math.random() * 50) + 1; 
 
             if (!this.arrayBombs.includes(nBombs)) {
                 this.arrayBombs.push(nBombs);
@@ -71,9 +77,26 @@ createApp({
     // controlla che la casella è inclusa nell'array delle bombe
     checkBomb(square) {
         if (this.arrayBombs.includes(square)) {
+
+            // game over
             this.foundBomb = true;
             this.gameOver = true;
+            alert('Hai perso! Il punteggio è di ' + (this.counter + 1));
         }
+        
+        if (this.counter < this.goodSqaures){
+
+            // punteggio aumenta
+            this.counter++;
+            console.log('Il counter è di ' + this.counter);
+            console.log('check di quelle buone ' + this.goodSqaures);
+        }
+        else if (this.counter == this.goodSqaures) {
+
+            // vittoria
+            alert('VITTORIA! Il tuo punteggio è di ' + (this.counter + 1));
+        }
+        
     },
     // aggiunge la classe rossa in base alle condizioni SOLO alle caselle bomba
     setRedClass(square){
@@ -86,6 +109,7 @@ createApp({
         this.foundBomb = false;
         this.gameOver = false;
         this.start = false;
+        this.goodSqaures = 0;
         this.arrayBombs = [];
     }
   },
