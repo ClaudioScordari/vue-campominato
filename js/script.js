81cce363bf0bc411d6d2f950471d23dcb8de923c
ClaudioScordari <scordari22@gmail.com>
2024-06-01 19:17:52 +0200

    Correzioni stile + funzione di start (in corso)

--- js/script.js
+++ js/script.js
@@ -13,6 +13,9 @@
     
     4 - reset del gioco
 
+    5 - do la possibilità di fissare la difficoltà in autonomia, ovvero è il player che fissa 
+        con quante bombe vuole giocare
+
 */
 
 const { createApp } = Vue
@@ -21,7 +24,7 @@ createApp({
   data() {
     return {
         squares: [
-            0,   1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,
+            1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,
             13,  14,  15,  16,  17,  18,  19,  20,  21,  22,  23,  24,  25,
             26,  27,  28,  29,  30,  31,  32,  33,  34,  35,  36,  37,  38,
             39,  40,  41,  42,  43,  44,  45,  46,  47,  48,  49,  50,  51,
@@ -33,18 +36,31 @@ createApp({
         arrayBombs: [],
         gameOver: false,
         foundBomb: false,
+        start: false,
+        inputBombs: 15,
     }
   },
   methods: {
+    // fa apparire il tabellone
+    startGame(){
+        this.start = true;
+
+        // riempio l'array delle bombe
+        this.setBombs(this.inputBombs);
+    },
     // riempire l'array delle bombe solo da numeri diversi
-    setBombs(max) {
-        while (this.arrayBombs.length <= 10) {
-            let nBombs = Math.floor(Math.random() * max) + 1;
+    setBombs() {
+        while (this.arrayBombs.length <= this.inputBombs) {
+
+            // si arriva ad un massimo di 100 il valor che può avere la casella con la bomba
+            let nBombs = Math.floor(Math.random() * 100) + 1; 
 
             if (!this.arrayBombs.includes(nBombs)) {
                 this.arrayBombs.push(nBombs);
             }
         }
+
+        console.log(this.arrayBombs);
     },
     // controlla che la casella è inclusa nell'array delle bombe
     checkBomb(square) {
@@ -63,13 +79,13 @@ createApp({
     resetGame(){
         this.foundBomb = false;
         this.gameOver = false;
+        this.start = false;
         this.arrayBombs = [];
-        this.setBombs(50); // si ottinene di nuovo l'array delle bombe
+        this.setBombs(this.inputBombs); // si ottinene di nuovo l'array delle bombe
         console.log(this.arrayBombs);
     }
   },
   mounted(){
-    this.setBombs(50);
-    console.log(this.arrayBombs);
+    // ...
   },
 }).mount('#app')
